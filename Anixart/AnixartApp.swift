4fcd8435e6dce4f8e91b88964b81dbd2b654ad25
Shirahemu <azalan056.ru@gmail.com>
2026-06-26 20:41:23 +0300

    Initial Commit

---
+++ Anixart/AnixartApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnixartApp.swift
+//  Anixart
+//
+//  Created by Shirahemu on 26.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct AnixartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
